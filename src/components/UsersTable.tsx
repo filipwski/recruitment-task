50b87fb94339e69491db5b14e2d36df089e81d0d
Filip Skibiński <filipwski@gmail.com>
2021-12-09 04:37:04 +0000

    correct spacing

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -4,8 +4,8 @@ import { getUsers } from '../clients/apiClient';
 export const UsersTable = () => {
     const { error, loading, value } = useAsync(getUsers, []);
     return loading
-            ? (<p>Loading...</p>)
-            : error
-            ? (<p>error.message</p>)
-            : (<p>Users</p>);
+        ? (<p>Loading...</p>)
+        : error
+        ? (<p>error.message</p>)
+        : (<p>Users</p>);
 };
